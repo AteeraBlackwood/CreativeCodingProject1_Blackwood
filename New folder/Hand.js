26ce93f483ea5168f12ef971e35c3a32bf02f6a6
AteeraBlackwood <aeb734@nyu.edu>
2020-10-25 22:52:53 -0400

    Sunday Night Update
    circles are crooked for some reason

---
+++ New folder/Hand.js
@@ -0,0 +1,11 @@
+function hand(){//the ellipses are fingers, the circle is the palm
+	noStroke();
+	fill(237, 209, 157);//a flesh tone
+	circle(350,750,300);
+	rotate(PI/33);
+  ellipse(300,540,75,200);
+  ellipse(380,500,75,200);
+  ellipse(460,500,75,200);
+  ellipse(540,540,75,200);
+  ellipse(580,700,200,75);
+}
--- New folder/Project Process 2 Update.js
+++ New folder/Project Process 2 Update.js
@@ -1,8 +1,20 @@
-//PVector location = new PVector(100,100);
-//PVector velocity = new PVector(1,3.3);
 
-//break; gets you out of switch(){}
-
+function circle1(){
+  circle(width/2, height/2,circleSizeX ,circleSizeY);
+} 
+function circle2(){
+  circle(width/2, height/2,circleSizeX ,circleSizeY);
+}
+function circle3(){
+  circle(width/2, height/2,circleSizeX ,circleSizeY);
+}
+function circle4(){
+circle(width/2, height/2,circleSizeX ,circleSizeY);
+}
+function circle5(){
+circle(width/2, height/2,circleSizeX ,circleSizeY);
+}
+function circle6(){
 function circle1(){
   circle(width/2, height/2,circleSizeX ,circleSizeY);
 } 
@@ -31,6 +43,10 @@ var circleSizeY = 25;
 var x, y;
 var lightWidth=300
 var lightHeight=300
+var a,b;
+let vb=0; //is velocity usually vy
+a=500
+b=100
 
 function setup() {
   createCanvas(1000, 1000);
@@ -87,13 +103,11 @@ circle7(width/2, height/2,circleSizeX ,circleSizeY);}
   circleSizeY++; 
   }
   //part 2
-  if(mouseIsPressed){//this kinda changes the scene since the perspective is different
+  if(circleSizeX>=1000){//this kinda changes the scene since the perspective is different
   //I want this part to start when the final circle fills the screen but this command would require specific timing
-    
-    
       noStroke();
       fill(255);
-      circle(650,150,lightWidth,lightHeight);// borrowed idea from https://www.openprocessing.org/sketch/466530
+      ellipse(650,150,lightWidth,lightHeight);// borrowed idea from https://www.openprocessing.org/sketch/466530
       lightWidth=lightWidth-5;
       lightHeight=lightHeight-5;
     hand();
@@ -101,77 +115,14 @@ circle7(width/2, height/2,circleSizeX ,circleSizeY);}
   background(0);
     }
   }
+  if(lightWidth<20){
+  faller();
+  b+=2; 
+    //if(b>=1000){
+      //vb=0;
+  
+    //}
+    circleLoop();
+      }
+  
 }
-  
-
-  
-//}
-
-//function applyForce(){
-  //acceleration =force;
-//}
-
-
-
-
-
-
-
-
-
-//class Liquid {
-//The liquid object includes a variable defining its coefficient of drag.
-  //float x,y,w,h;
-  //float c;
- 
-  //Liquid(float x_, float y_, float w_, float h_, float c_) {
-   // x = x_;
-    //y = y_;
-   // w = w_;
-    //h = h_;
-    //c = c_;
-  //}
- 
-  //function display() {
-   // noStroke();
-    //fill(1, 1, 20,50);
-   // rect(x,y,w,h);
- // }
- 
-//}
-//Liquid liquid;
- 
-//function setup() {
-//Initialize a Liquid object. Note the coefficient is low (0.1), otherwise the object would come to a halt fairly quickly (which may someday be the effect you want).
-  //liquid = new Liquid(0, height/1.3, width, height/1.3, 0.1);// I want the liquid to take up more of the screen
-//}
-//if (movers[i].isInside(liquid)) {
-//If a Mover is inside a Liquid, apply the drag force.
- // movers[i].drag(liquid);
-//}
-//boolean isInside(Liquid l) {
-//This conditional statement determines if the PVector location is inside the rectangle defined by the Liquid class.
- // if (location.x>l.x && location.x<l.x+l.w && location.y>l.y && location.y<l.y+l.h)
- // {
-  //  return true;
-  //} else {
- //   return false;
-  //}
-//}
-//void drag(Liquid l) {
- 
-    //float speed = velocity.mag();
-//The force’s magnitude: Cd * v~2~
-    //float dragMagnitude = l.c * speed * speed;
- 
-    //PVector drag = velocity.get();
-    //drag.mult(-1);
-//The force's direction: -1 * velocity
-    //drag.normalize();
- 
-//Finalize the force: magnitude and direction together.
-    //drag.mult(dragMagnitude);
- 
-//Apply the force.
-    //applyForce(drag);
-  //}
